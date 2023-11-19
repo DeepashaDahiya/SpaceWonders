888365855b1f6d4422b497f5c921c8466327bbb6
DeepashaDahiya <144469025+DeepashaDahiya@users.noreply.github.com>
2023-11-19 21:50:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("I like pizza.");
+console.log("It's really good!");
+
+window.alert("I REally love pizza...");
+
+// this is a comment
+// a multiline
+//  comment
+
+let age;//numbers
+age=23
+age=age+1;
+let firstname = "bro";//strings
+let student=true;//booleans
+
+console.log(firstname);
+console.log(age);
+console.log(student);
+
+
+console.log("Hello",firstname);
+console.log("you are", age, "years old!");
+console.log("Enrolled:",student);
+
